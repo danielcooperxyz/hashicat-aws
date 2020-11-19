4da88f9d0be4c2aca781cda362e67218a14e064c
Daniel Cooper <daniel.cooper@qad.re>
2020-11-19 11:55:03 +0000

    provision the s3 bucket correctly

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,10 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+
+module "s3-bucket" {
+  source  = "app.terraform.io/personal-org-123/s3-bucket/aws"
+  version = "1.15.0"
+  # insert required variables here
+
   bucket_prefix = "danielcooper"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
 }
+
